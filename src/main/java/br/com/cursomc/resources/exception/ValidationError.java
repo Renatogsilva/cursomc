ede3c8f49ea2ebbeffe2cf8c0e09dd0459e6681a
renato <renato.gomes@haslife.com.br>
2020-02-12 00:23:51 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/br/com/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,22 @@
+package br.com.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandartError{
+	private static final long serialVersionUID = 1L;
+
+	public ValidationError(Integer status, String message, Long timeStamp) {
+		super(status, message, timeStamp);
+	}
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+}
